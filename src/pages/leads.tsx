ed562a67ea3ed2730dd6683508b8352d8ef7b3bd
TuaSmile <tuasmiledev@outlook.com>
2025-03-07 14:00:06 +0200

    commit lead page update

--- src/pages/leads.tsx
+++ src/pages/leads.tsx
@@ -1,19 +1,38 @@
+import { ReactNode } from "react";
 import Navbar from "@/components/layout/Navbar";
 import Sidebar from "@/components/layout/Sidebar";
-import { ReactNode } from "react";
 
-export default function Leads() {
-  return <>This page will be implemented soon.</>;
+// Type for page layout
+interface LayoutProps {
+  children: ReactNode;
 }
 
+const Leads = () => {
+  return (
+    <div className="content-container">
+      <h1 className="page-header">Leads Page - Coming Soon</h1>
+      <p className="text-center">This page will be implemented soon.</p>
+    </div>
+  );
+};
+
+// Define the layout for this page
 Leads.getLayout = (page: ReactNode) => {
   return (
-    <div className="flex h-full">
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar Component */}
       <Sidebar />
-      <div className="main-content flex-1">
+
+      {/* Main Content Area */}
+      <div className="flex-1 flex flex-col">
+        {/* Navbar Component */}
         <Navbar tab="Leads" />
-        {page}
+        
+        {/* Main Page Content */}
+        <main className="flex-1 p-6">{page}</main>
       </div>
     </div>
   );
 };
+
+export default Leads;
